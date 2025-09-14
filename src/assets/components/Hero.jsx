3a7702b9158d2b18d8b76781df22caa87978c620
Mathias <mathiayuba@gmail.com>
2025-09-15 00:26:12 +0100

    Hero section responsive text downscale

--- src/assets/components/Hero.jsx
+++ src/assets/components/Hero.jsx
@@ -5,12 +5,12 @@ import { BiArrowToRight } from "react-icons/bi";
 const Hero = () => {
   return (
     <section 
-        className="h-[91vh] bg-cover bg-[50%_60%] text-white flex flex-col items-center justify-center gap-2 text-center"
+        className="h-[91vh] bg-cover bg-center md:bg-[50%_60%] text-white flex flex-col items-center justify-center gap-2 text-center"
         style={{ backgroundImage: `url(${heroImage})` }}>
     
-        <h1 className='font-bold text-7xl/24 text-shadow-heading text-balance w-xl'>Welcome to CampusConnect</h1>
-        <h2 className='font-light tracking-wide text-2xl'>Your Gateway to Campus Events</h2>
-        <p className='font-thin text-white/70 tracking-widest'>Discover, explore and participate in existing college events</p>
+        <h1 className='font-bold text-5xl/16 md:text-7xl/24 text-shadow-heading text-balance w-md md:w-xl'>Welcome to CampusConnect</h1>
+        <h2 className='font-light tracking-wide text-lg md:text-2xl'>Your Gateway to Campus Events</h2>
+        <p className='font-thin text-white/70 tracking-widest text-sm md:text-md'>Discover, explore and participate in existing college events</p>
         <button className='mt-8 bg-[#0056D2] rounded-lg px-4 py-2 flex items-center justify-center font-medium'>Learn More <BiArrowToRight className='ml-1'/></button>
     </section>
   )
--- src/assets/components/HomeMetrics.jsx
+++ src/assets/components/HomeMetrics.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const HomeMetrics = () => {
   return (
-    <section className='h-[20vh] flex items-center justify-center text-center gap-16 text-lg font-semibold font-stretch-extra-expanded'>
+    <section className='min-h-[20vh] py-12 px-8 flex flex-wrap items-center justify-center text-center gap-16 text-lg font-semibold font-stretch-extra-expanded'>
       <div><span className='text-[#0056D2] block text-4xl'>50+</span>Annual Events</div>
       <div><span className='text-[#0C942A] block text-4xl'>5000+</span>Active Students</div>
       <div><span className='text-[#D31470] block text-4xl'>25+</span>Departments</div>
