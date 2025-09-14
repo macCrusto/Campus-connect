595cbda5fc0dc31fb653fc631920eed357a58a41
Mathias <mathiayuba@gmail.com>
2025-09-14 23:32:59 +0100

    Fixed minor HomePage bug with component imports

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import Navbar from './assets/components/Navbar.jsx'
-import Hero from './assets/components/Hero.jsx'
-import HomeMetrics from './assets/components/HomeMetrics.jsx'
+import Navbar from '../assets/components/Navbar.jsx'
+import Hero from '../assets/components/Hero.jsx'
+import HomeMetrics from '../assets/components/HomeMetrics.jsx'
 
 const HomePage = () => {
   return (
